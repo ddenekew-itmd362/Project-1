b12dc168c1ac032296c77344018c50618439d49b
Dani_Exodus <ddenekew@hawk.iit.edu>
2019-03-06 23:40:22 -0600

    update site.js

--- js/site.js
+++ js/site.js
@@ -1,3 +1,9 @@
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
+
 document.getElementById("submit").addEventListener("click", checkbox);
 function checkForm(form)
 {
